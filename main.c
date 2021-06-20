057c18e3a25a9b877c9431475619f085f1624de8
Amir Esmaeili <amiresm17@gmail.com>
2021-06-20 12:50:45 +0430

    added mpich func

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <mpi.h>
 
 void display(int *array, int size)
 {
@@ -52,7 +53,6 @@ void mergeArrays(int *array1, int size1, int *array2, int size2, int *merged)
     {
         merged[k++] = array2[j++];
     }
-
 }
 
 int main()
@@ -68,10 +68,14 @@ int main()
 
     printf("size1: %d, size2: %d\n", size1, size2);
 
-    for (size_t i = 0; i < size; i++) {
-        if (i < size1) {
+    for (size_t i = 0; i < size; i++)
+    {
+        if (i < size1)
+        {
             arr1[i] = unsortedArr[i];
-        } else {
+        }
+        else
+        {
             arr2[i - size1] = unsortedArr[i];
         }
     }
@@ -85,4 +89,93 @@ int main()
     display(merged, size);
 
     return 0;
+}
+
+int main(int argc, char **argv)
+{
+
+    // Initialize the MPI environment
+    MPI_Init(NULL, NULL);
+    // Find out rank, size
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // We are assuming at least 3 processes for this task
+    if (world_size < 3)
+    {
+        fprintf(stderr, "World size must be greater than 2 for %s\n", argv[0]);
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+    int size = 14;
+    int input[] = {1, 2, 5, 7, 3, 4, 10, 8, 5, 3, 2, 11, 12, 10};
+    int size1 = size / 2;
+    int size2 = size - size1;
+
+    if (world_rank == 0)
+    {
+        //provide data for world1
+
+        int arr1[size1];
+        int arr2[size2];
+
+        for (size_t i = 0; i < size; i++)
+        {
+            if (i < size1)
+            {
+                arr1[i] = input[i];
+            }
+            else
+            {
+                arr2[i - size1] = input[i];
+            }
+        }
+
+        //send the first half to world1
+        MPI_Send(arr1, size1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+
+        //send the second half to world2
+        MPI_Send(arr2, size2, MPI_INT, 2, 0, MPI_COMM_WORLD);
+
+        //recieve world1 result
+        int world1_result[size1];
+        MPI_Recv(&world1_result, size1, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        //recieve world2 result
+        int world2_result[size2];
+        MPI_Recv(&world2_result, size2, MPI_INT, 2, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        int result[size];
+        mergeArrays(world1_result, size1, world2_result, size2, result);
+
+        display(result, size);
+    }
+    else if (world_rank == 1)
+    {
+        //recieve my_data from world0
+        int my_data[size1];
+        MPI_Recv(my_data, size1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        //calculation
+        sortArray(my_data, size1);
+        display(my_data, size1);
+
+        //sending data back to world0
+        MPI_Send(&my_data, size1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    }
+    else
+    {
+        //recieve my_data from world0
+        int my_data[size2];
+        MPI_Recv(my_data, size2, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        //calculation
+        sortArray(my_data, size2);
+        display(my_data, size2);
+
+        //sending data back to world0
+        MPI_Send(&my_data, size2, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    }
+    MPI_Finalize();
 }
